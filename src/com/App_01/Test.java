8245feeb48459236ea0c91630a646c329a3b713e
Denis Nikashin <denis.n.82.dn@gmail.com>
2016-11-03 21:49:04 +0200

    first commit

---
+++ src/com/App_01/Test.java
@@ -0,0 +1,10 @@
+package com.App_01;
+
+/**
+ * Created by denisnikashin on 02.11.16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Sheet, I do it !!!!");
+    }
+}
