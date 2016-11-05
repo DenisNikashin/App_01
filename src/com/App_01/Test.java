6729709a74f10d0a143b1742eb145431b27f09ae
Denis Nikashin <denis.n.82.dn@gmail.com>
2016-11-05 14:29:07 +0200

    third commit

--- src/com/App_01/Test.java
+++ src/com/App_01/Test.java
@@ -6,5 +6,6 @@ package com.App_01;
 public class Test {
     public static void main(String[] args) {
         System.out.println("At last, I did it !!!!");
+        System.out.println("I made changes 04.11.16");
     }
 }
